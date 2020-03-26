a2217c23212da1d885e27047435e5f3ec127b8e8
Marcin Możejko <marcin@sigmoidal.io>
2020-03-26 10:45:26 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+      name='AMP',
+      version='0.1',
+      description='Python package for peptide generation',
+      author='Paulina Szymczak',
+      author_email='to@do.net',
+      url='https://www.to.do.net/',
+      packages=['amp'],
+)
