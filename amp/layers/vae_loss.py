4d05cb224a9bffb8069ac6ef61a8a6d29971276f
Marcin Możejko <marcin@sigmoidal.io>
2020-03-30 18:16:47 +0000

    Layers

---
+++ amp/layers/vae_loss.py
@@ -0,0 +1,37 @@
+from keras import backend
+from keras import layers
+from keras import metrics
+import tensorflow as tf
+
+
+class VAELoss(layers.Layer):
+
+    def __init__(
+            self,
+            kl_weight: tf.Variable,
+            **kwargs):
+        self.kl_weight = kl_weight
+        super(VAELoss, self).__init__(**kwargs)
+
+    def calculate_loss(
+            self,
+            x,
+            generated_x,
+            z_sigma,
+            z_mean,
+    ):
+        rcl = backend.mean(metrics.sparse_categorical_crossentropy(x, generated_x), axis=-1)
+        kl_loss = - 0.5 * backend.sum(1 + z_sigma -
+                                      backend.square(z_mean) -
+                                      backend.exp(z_sigma),
+                                      axis=-1)
+        return rcl + (self.kl_weight * kl_loss)
+
+    def call(self, inputs):
+        x = inputs[0]
+        generated_x = inputs[1]
+        z_sigma = inputs[2]
+        z_mean = inputs[3]
+        loss = self.calculate_loss(x, generated_x, z_sigma, z_mean)
+        self.add_loss(loss, inputs=inputs)
+        return x
