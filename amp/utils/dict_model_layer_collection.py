180f81fba1a2e9f146aa762f83de963d3c5a4feb
Marcin Możejko <marcin@sigmoidal.io>
2020-04-06 19:44:50 +0200

    Added Dict model layer collection/

--- amp/utils/dict_model_layer_collection.py
+++ amp/utils/dict_model_layer_collection.py
@@ -34,10 +34,38 @@ class DictModelLayerCollection(model.ModelLayerCollection):
         config_path = os.path.join(path, self.LAYER_CONFIG_NAME)
         with open(config_path, 'w') as json_handle:
             config = {
-                'type': type(layer).__name__,
-                'layer_config': layer.get_config(),
+                'class_name': layer.__class__.__name__,
+                'config': layer.get_config(),
             }
             json.dump(config, json_handle)
             for layer_nb, weight in enumerate(layer.get_weights()):
                 current_weight_path = os.path.join(path, f'{layer_nb}{self.WEIGHT_NAME}')
                 np.save(current_weight_path, weight)
+
+    @classmethod
+    def load(cls, path: str) -> "DictModelLayerCollection":
+        files_in_dir = os.listdir(path)
+        full_paths = [os.path.join(path, file_path) for file_path in files_in_dir]
+        full_subdirs = [(path, name) for path, name in zip(full_paths, files_in_dir) if os.path.isdir(path)]
+        return cls(
+            dict_to_layer={
+                name: cls._load_layer_from_path(path) for path, name in full_subdirs
+            }
+        )
+
+    @classmethod
+    def _load_layer_from_path(cls, path):
+        config_dict_path = os.path.join(path, cls.LAYER_CONFIG_NAME)
+        with open(config_dict_path, 'r') as json_handle:
+            config = json.load(json_handle)
+        layer = layers.deserialize(
+            config=config,
+        )
+        weight_paths = [
+            os.path.join(path, weight_name)
+            for weight_name in os.listdir(path) if weight_name.endswith(cls.WEIGHT_NAME)
+        ]
+        weights = [np.load(weight_path) for weight_path in weight_paths]
+        layers.Layer.loaded_weights = None
+        layers.Layer.loaded_weights = weights
+        return layer
