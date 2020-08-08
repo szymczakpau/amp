0d177bbdd229f2d4499a402124d04605a9065ec1
szymczakpau <szymczak.pau@gmail.com>
2020-08-08 23:06:54 +0200

    Clean up the classifier logger

--- amp/utils/classifier_utils.py
+++ amp/utils/classifier_utils.py
@@ -1,5 +1,4 @@
 from keras import callbacks
-from keras import backend
 import sklearn.metrics as metrics
 import numpy as np
 
@@ -23,21 +22,6 @@ class ClassifierLogger(callbacks.Callback):
         self.fn = out_fn
         self.patience = patience
         self.no_improve = 0
-
-    # def on_train_begin(self, logs={}):
-    #     return
-    #
-    # def on_train_end(self, logs={}):
-    #     return
-    #
-    # def on_epoch_begin(self, epoch, logs={}):
-    #     return
-    #
-    # def on_batch_begin(self, batch, logs={}):
-    #     return
-    #
-    # def on_batch_end(self, batch, logs={}):
-    #     return
 
     def on_epoch_end(self, epoch):
         cv_pred = self.model.predict(self.validation_data[0], batch_size=1024)
