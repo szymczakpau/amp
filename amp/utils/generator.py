92ddbe620cb94edb8c04dd9bcd037733946a1786
szymczakpau <szymczak.pau@gmail.com>
2020-08-29 14:49:38 +0200

    Modify generator to simpler solution

--- amp/utils/generator.py
+++ amp/utils/generator.py
@@ -1,67 +1,43 @@
-import csv
+from itertools import cycle, islice
 
-from keras.preprocessing import sequence
-from keras.preprocessing import text
 import numpy as np
 
-VOCAB_SIZE = 20
-MAX_LENGTH = 100
+def array_generator(array, batch_size):
+    c = cycle(array)
+    while True:
+        yield np.array(list(islice(c, batch_size)))
 
 
-class UniprotGenerator:
+def concatenated_generator(
+        uniprot_x,
+        uniprot_y,
+        amp_x,
+        amp_y,
+        batch_size,
+):
+    amp_x_gen = array_generator(amp_x, batch_size)
+    amp_y_gen = array_generator(amp_y, batch_size)
+    uniprot_x_gen = array_generator(uniprot_x, batch_size)
+    uniprot_y_gen = array_generator(uniprot_y, batch_size)
 
-    def __init__(
-            self,
-            path,
-            batch_size,
-            nb_of_seqs_in_file: int,
-            max_len: int = MAX_LENGTH,
-            vocab_size: int = VOCAB_SIZE,
-    ):
-        self.nb_of_seqs_in_file = nb_of_seqs_in_file
-        self.path = path
-        self.batch_size = batch_size
-        self.vocab_size = vocab_size
-        self.max_len = max_len
+    while True:
+        batch_amp_x = next(amp_x_gen)
+        batch_amp_y = next(amp_y_gen)
 
-    def __len__(self):
-        return int(np.floor(self.nb_of_seqs_in_file / self.batch_size))
+        batch_uniprot_x = next(uniprot_x_gen)
+        batch_uniprot_y = next(uniprot_y_gen)
 
-    def __iter__(self):
-        """Generate one batch of data"""
-        # Generate indices of the batch
-        while True:
-            current_file_index = 0
-            with open(self.path, 'r') as csv_file:
-                reader = csv.reader(csv_file)
-                current_batch = []
-                current_batch_index = 0
-                for line_number, row in enumerate(reader):
-                    if line_number == 0:
-                        continue
-                    current_batch.append(row[1])  # Only sequence
-                    current_batch_index += 1
-                    current_file_index += 1
-                    if current_batch_index == self.batch_size:
-                        one_hot_batch = self.to_one_hot(current_batch)
-                        padded_batch = self.pad(one_hot_batch)
-                        yield padded_batch
-                        current_batch_index = 0
-                        current_batch = []
-                        if current_file_index + self.batch_size > self.nb_of_seqs_in_file:
-                            break
+        result_x = np.concatenate([batch_amp_x, batch_uniprot_x])
+        result_amp = np.concatenate([batch_amp_y, batch_uniprot_y])
 
-    def to_one_hot(self, x):
-        alphabet = list('ACDEFGHIKLMNPQRSTVWY')
-        classes = range(1, 21)
-        aa_encoding = dict(zip(alphabet, classes))
+        noise_in = np.random.normal(0, 0.1, size=(result_amp.shape[0], 64))
 
-        return [[aa_encoding[aa] for aa in seq] for seq in x]
-
-    def pad(self, x):
-        return sequence.pad_sequences(
-            x,
-            maxlen=self.max_len,
-            padding='post',
-            value=0.0
-        )
+        yield [
+                  result_x,
+                  result_amp,
+                  noise_in,
+              ], \
+              [
+                  result_amp,
+                  np.zeros_like(result_amp),
+              ]
