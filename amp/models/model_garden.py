e1c7cffebe3a5ac5a7b45fe82127d34a09b38051
szymczakpau <szymczak.pau@gmail.com>
2020-08-30 13:45:34 +0200

    Add new classifier to the model garden

--- amp/models/model_garden.py
+++ amp/models/model_garden.py
@@ -1,4 +1,4 @@
-from amp.models.discriminators import veltri_amp_classifier
+from amp.models.discriminators import veltri_amp_classifier, noconv_amp_classifier
 from amp.models.decoders import amp_decoder
 from amp.models.encoders import amp_encoder
 from amp.models.master import master
@@ -6,6 +6,7 @@ from amp.models.master import master
 
 MODEL_GAREDN = {
     'VeltriAMPClassifier': veltri_amp_classifier.VeltriAMPClassifier,
+    'NoConvAMPClassifier': noconv_amp_classifier.NoConvAMPClassifier,
     'AMPDecoder': amp_decoder.AMPDecoder,
     'AMPEncoder': amp_encoder.AMPEncoder,
     'MasterAMPTrainer': master.MasterAMPTrainer,
